520826847643af0aee37887c7cb3ed73931cf90d
Unknown <theorybreakerhacks@gmail.com>
2019-04-14 22:12:54 +0200

    Finish AdvHD unpacker

--- ArchiveUnpacker/Program.cs
+++ ArchiveUnpacker/Program.cs
@@ -16,6 +16,7 @@ namespace ArchiveUnpacker
             UnpackerRegistry.Register<AIMSUnpacker>(AIMSUnpacker.IsGameFolder);
             UnpackerRegistry.Register<RenPyUnpacker>(RenPyUnpacker.IsGameFolder);
             UnpackerRegistry.Register<MajiroArcUnpacker>(MajiroArcUnpacker.IsGameFolder);
+            UnpackerRegistry.Register<AdvHDUnpacker>(AdvHDUnpacker.IsGameFolder);
 
             Console.Write("Gimme directory: ");
 #if DEBUG

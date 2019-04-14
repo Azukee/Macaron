42b4f2613a1a674a76f4b627110e8aefde3d6971
HoLLy <dario.db@hotmail.com>
2019-04-14 03:13:21 +0200

    Add some code that would probably work on unencrypted files

--- ArchiveUnpacker/Unpackers/CatSystem2Unpacker.cs
+++ ArchiveUnpacker/Unpackers/CatSystem2Unpacker.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using ArchiveUnpacker.Framework;
+using ArchiveUnpacker.Framework.Exceptions;
 
 namespace ArchiveUnpacker.Unpackers
 {
     /// <summary>
     /// Unpacker for CatSystem2
     ///
-    /// Used in games like Idol Magical Girl Chiruchiru Michiru part 2.
+    /// Used in games like Idol Magical Girl Chiruchiru Michiru part 1/2.
     /// </summary>
     public class CatSystem2Unpacker : IUnpacker
     {
@@ -20,7 +23,21 @@ namespace ArchiveUnpacker.Unpackers
 
         public IEnumerable<IExtractableFile> LoadFilesFromArchive(string inputArchive)
         {
-            throw new System.NotImplementedException();
+            using (var fs = File.OpenRead(inputArchive))
+            using (var br = new BinaryReader(fs)) {
+                string magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                if (magic != FileMagic)
+                    throw new InvalidMagicException();
+
+                int unk1 = br.ReadInt32();
+                for (int i = 0; i < unk1 + 1; i++) {
+                    string fileName = Encoding.ASCII.GetString(br.ReadBytes(0x40)).TrimEnd('\0');
+                    uint pos = (br.ReadUInt32() ^ 0) * 0x48 + 8;
+                    uint len = br.ReadUInt32() ^ 0;
+                }
+            }
+            
+            throw new NotImplementedException();
         }
 
         private IEnumerable<string> GetArchivesFromGameFolder(string gameDirectory) => Directory.GetFiles(gameDirectory, "*.int").Where(FileStartsWithMagic);

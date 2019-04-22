f1af33aaa80dcd2532e2e94a61ad6f1331cc1005
HoLLy <dario.db@hotmail.com>
2019-04-22 13:48:32 +0200

    Clean some whitespace

--- ArchiveUnpacker/Utils/Extensions.cs
+++ ArchiveUnpacker/Utils/Extensions.cs
@@ -32,10 +32,11 @@ namespace ArchiveUnpacker.Utils
             return name;
         }
 
-        public static int GetBits(this BinaryReader br, int count, ref int cBits, ref int bits) {
+        public static int GetBits(this BinaryReader br, int count, ref int cBits, ref int bits)
+        {
             while (cBits < count) {
                 int b;
-                if ((b = br.ReadByte()) == -1) 
+                if ((b = br.ReadByte()) == -1)
                     return -1;
                 bits = (bits << 8) | b;
                 cBits += 8;
@@ -45,42 +46,42 @@ namespace ArchiveUnpacker.Utils
             cBits -= count;
             return (bits >> cBits) & rot;
         }
-        
+
         public static int ReadInt32BE(this BinaryReader br)
         {
             var data = br.ReadBytes(4);
             Array.Reverse(data);
             return BitConverter.ToInt32(data, 0);
         }
-        
+
         public static uint ReadUInt32BE(this BinaryReader br)
         {
             var data = br.ReadBytes(4);
             Array.Reverse(data);
             return BitConverter.ToUInt32(data, 0);
         }
-        
+
         public static short ReadInt16BE(this BinaryReader br)
         {
             var data = br.ReadBytes(2);
             Array.Reverse(data);
             return BitConverter.ToInt16(data, 0);
         }
-        
+
         public static ushort ReadUInt16BE(this BinaryReader br)
         {
             var data = br.ReadBytes(2);
             Array.Reverse(data);
             return BitConverter.ToUInt16(data, 0);
         }
-        
+
         public static long ReadInt64BE(this BinaryReader br)
         {
             var data = br.ReadBytes(8);
             Array.Reverse(data);
             return BitConverter.ToInt64(data, 0);
         }
-        
+
         public static float ReadSingleBE(this BinaryReader br)
         {
             var data = br.ReadBytes(4);
